531fd5b30a54d00da8cb2eb5c95a33ffb60a8de1
DenseFog <move11@126.com>
2023-12-25 17:36:49 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 9008;
+
+app.get('/status', async (req, res) => {
+    const apiKey = req.query.api_key; // 获取 api_key 参数的值
+
+    if (!apiKey) {
+        return res.status(400).json({ error: 'api_key parameter is missing' });
+    }
+    try {
+        const response = await axios.post('https://api.uptimerobot.com/v2/getMonitors', {
+            api_key: apiKey,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Proxy server is running on port ${port}`);
+});
