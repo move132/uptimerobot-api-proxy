a35808ace68f70f39f0819feeb2c3c65343575b3
DenseFog <move11@126.com>
2023-12-26 10:40:12 +0800

    feat: 调整代码

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,37 @@
+# FROM node:16
+
+# RUN npm install -g pnpm && npm install -g tsup
+
+# WORKDIR /app
+
+# COPY ./src/index.ts package.json tsup.config.ts tsconfig.json .
+# RUN pnpm install && pnpm run build
+
+# EXPOSE 9008
+
+# CMD [ "node", "./dist/index.js" ]
+
+# 设置基础镜像
 FROM node:16
 
-RUN npm install -g pnpm && npm install -g tsup
-
+# 设置工作目录
 WORKDIR /app
 
-COPY ./src/index.ts package.json tsup.config.ts tsconfig.json .
-RUN pnpm install && pnpm run build
+# 复制 package.json 和 pnpm-lock.yaml
+COPY package.json pnpm-lock.yaml /app/
 
-EXPOSE 9008
+# 安装依赖
+RUN npm install -g pnpm && pnpm install
 
-CMD [ "node", "./dist/index.js" ]
+# 复制其他文件
+COPY . /app
+
+# 构建项目
+RUN npm run build
+
+# 暴露端口
+EXPOSE 3006
+ENV PORT 3006
+ENV BASE_URL https://api.uptimerobot.com/v2
+# 运行应用
+CMD ["node", "dist/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
-import express from 'express';
-import axios from 'axios';
-import cors from 'cors';
+import express from 'express'
+import axios from 'axios'
+import cors from 'cors'
 
-const app = express();
-const port = 9008;
-app.use(cors()); // 启用 CORS 中间件
+const app = express()
+const port = process.env.PORT || 3006
+const baseUrl = process.env.BASE_URL || 'https://api.uptimerobot.com/v2'
 
-app.get('/status', async (req, res) => {
-    const apiKey = req.query.api_key; // 获取 api_key 参数的值
+app.use(cors()) // 启用 CORS 中间件
 
-    if (!apiKey) {
-        return res.status(400).json({ error: 'api_key parameter is missing' });
-    }
-    try {
-        const response = await axios.post('https://api.uptimerobot.com/v2/getMonitors', {
-            api_key: apiKey,
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        });
+app.get('/api/*', async (req, res) => {
+	const apiKey = req.query.api_key // 获取 api_key 参数的值
+	const apiUrl = req.url.replace('/api', '') // 去除路径中的/v2前缀
+	const proxyUrl = `${baseUrl}${apiUrl}`
+	if (!apiKey) {
+		return res.status(400).json({code: 400, msg: 'api_key parameter is missing'})
+	}
+	try {
+		const response = await axios.post(proxyUrl, {
+			api_key: apiKey,
+			...req.query,
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		})
 
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-    }
-});
+		res.json(response.data)
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({code: 500, msg: 'An error occurred'})
+	}
+})
 
 app.listen(port, () => {
-    console.log(`Proxy server is running on port ${port}`);
-});
+	console.log(`Proxy server is running on port ${port}`)
+})
